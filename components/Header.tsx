0c2fad2801a2cab1159ee011e23b1368df355bb9
ymc182 <ymc182@gmail.com>
2023-01-06 02:20:16 +1100

    railway

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,9 @@ import { CONTRACT_ID, useNearContext } from "../context/NearContext";
 
 export default function Header() {
 	const { modal, callMethods, viewMethod, selector } = useNearContext();
+
 	//dev-1668149532445-37289092214188
 	async function testTransaction() {
-		const { contract } = selector!.store.getState();
 		const res = await callMethods([
 			{
 				contractId: CONTRACT_ID,
